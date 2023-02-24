1f7b879f2f50e3ce0b7dd549554e2e26d35b8d25
kthin <kathythin@yahoo.com>
2023-02-23 18:24:48 -0800

    header component

--- my-app/src/component/HeaderBar.js
+++ my-app/src/component/HeaderBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export function HeaderBar(props) {
+export default function HeaderBar(props) {
     return (
         <div className="text-light bg primary p-1">
             <h1>ShopSpree</h1>
+            <p className="lead">Discover the difference with ShopSpree. Absolutely. Positively. Perfect.</p>
         </div>
-    );
+    )
 }
