583caab84d71a7c79b4d107b3d5bd8c750978aa8
Erica Gordy <egordy@uw.edu>
2023-02-25 12:52:56 -0800

    moving app.js to src, commented out category and brands, fixed firebase

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //css 
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import App from '.my-app/src/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
