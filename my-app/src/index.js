c3e3f12ca0c8a2ce5207d3bfa4cd14af5d0c95fa
sabatabatabai <saba5863@gmail.com>
2023-02-26 01:58:07 -0800

    brand data json

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './component/App';
 
+import BRAND_INFO from './data/brandinfo.json'; //the data to display
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App brandInfo={BRAND_INFO}/>
   </React.StrictMode>
 );
