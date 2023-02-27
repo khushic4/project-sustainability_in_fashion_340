31acf8bf721ebdf973059b537659b1013c3a7cab
Ansh Vidyarthi <avidyart@amazon.com>
2023-02-26 22:17:20 -0800

    Added price filter on brands without complete functionality

--- my-app/src/component/Brands.js
+++ my-app/src/component/Brands.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { BRANDS } from '../data/brandinfo';
 
+const prices = {
+    Low: 'S',
+    Medium: 'M',
+    High: 'L',
+    All: 'A'
+}
+
 export function Brands() {
     const {state} = useLocation();
+    const {price, setPrice} = useState<prices>(prices.All)
     const { categoryName } = state; // Read values passed on state
-    const brandCards = (category) => BRANDS.filter(brand => brand.category == category).map((brand) => {
+
+    const onPriceFilterClick = () => {
+        setPrice(prices.High)
+    }
+    const brandCards = (category) => BRANDS.filter(brand => (brand.category == category && (price == prices.All || brand.price == price))).map((brand) => {
         return (
             <div className="brand-container">
                 <img className="brand-image" src={brand.img} alt={brand.name}/>
@@ -22,6 +34,7 @@ export function Brands() {
     return (
         <div>
             <h1 style={{ margin: "2em" }}>{categoryName}</h1>
+            <button onClick = {onPriceFilterClick} > Set Price Filter</button>
             <div className="brand">
                 {brandCards(categoryName)}
             </div>
