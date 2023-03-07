5252e6056e31aeb0d48821c99fc5217dcf6f2ad8
sabatabatabai <saba5863@gmail.com>
2023-03-06 23:58:40 -0800

    all brand images showing

--- my-app/src/component/BrandCard.js
+++ my-app/src/component/BrandCard.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import BRAND_INFO from './brandinfo.js'
-import { useParams } from 'react-router-dom';
-import _ from 'lodash';
+// import React from 'react';
+// import BRAND_INFO from './brandinfo.js'
+// import { useParams } from 'react-router-dom';
+// import _ from 'lodash';
 
-export function BrandCard(props) {
-    const { brandData } = props;
-    //const {name, img, price, description, link} = brandData;
-    const { brandName } = useParams(); //REPLACE THIS WITH CORRECT VALUE
+// export function BrandCard(props) {
+//     const { brandData } = props;
+//     //const {name, img, price, description, link} = brandData;
+//     const { brandName } = useParams(); //REPLACE THIS WITH CORRECT VALUE
 
-    //pretend we loaded external data    
-    let brand =  _.find(BRAND_INFO, {name: brandName}); //find brand in data
+//     //pretend we loaded external data    
+//     let brand =  _.find(BRAND_INFO, {name: brandName}); //find brand in data
 
-    return (
-        <div className="brand-container">
-                <img className="brand-image" src={brand.img} alt={brand.name}/>
-                <div className="brand-middle">
-                    <p>{brand.description}</p>
-                    <a href={brand.link} target="_blank">
-                        <div className="brand-text">shop now</div>
-                    </a>
-                </div>
-            </div>
-    );
-}
+//     return (
+//         <div className="brand-container">
+//                 <img className="brand-image" src={brand.img} alt={brand.name}/>
+//                 <div className="brand-middle">
+//                     <p>{brand.description}</p>
+//                     <a href={brand.link} target="_blank">
+//                         <div className="brand-text">shop now</div>
+//                     </a>
+//                 </div>
+//             </div>
+//     );
+// }
 
 
 
--- my-app/src/component/BrandList.js
+++ my-app/src/component/BrandList.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import BrandCard from './BrandCard';
-import { useParams } from 'react-router-dom';
+// import React from 'react';
+// import BrandCard from './BrandCard';
+// import { useParams } from 'react-router-dom';
 
-export function BrandList(props) {
-    const { brands, brandData } = props;
-    const brandCards = brands.map((brand) => {
-        return (
-            <BrandCard key={brand.id} brandData={brand}/>
-        );
-    });
+// export function BrandList(props) {
+//     const { brands, brandData } = props;
+//     const brandCards = brands.map((brand) => {
+//         return (
+//             <BrandCard key={brand.id} brandData={brand}/>
+//         );
+//     });
 
-    return (
-        <div>
-            <h1 style={{ margin: "2em" }}>{brandData.category}</h1>
-            <div className="brand">
-                {brandCards}
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1 style={{ margin: "2em" }}>{brandData.category}</h1>
+//             <div className="brand">
+//                 {brandCards}
+//             </div>
+//         </div>
+//     )
+// }
 
 
 
